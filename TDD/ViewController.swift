6ebf716790a56aa09eadd03f24b1b7d00a46182b
gR <Gr>
2021-02-19 21:17:09 +0300

    add hello world

--- TDD/ViewController.swift
+++ TDD/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print ("hello world")
     }
 
     @IBAction func LogInButtonPressed(_ sender: Any) {
